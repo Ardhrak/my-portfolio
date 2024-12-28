642ac336ac9e602efc5e839b4d782d5ee52329a4
Ardhrak <159452892+Ardhrak@users.noreply.github.com>
2024-12-28 16:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+}
+
+    const typed = new typed ('multiple-text', {
+      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      typeSpeed: 50,
+    });
